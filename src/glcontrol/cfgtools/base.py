a1b1572e10edcbd64121ccf55b83e17672d68fd2
Leo Dirac <leo@groundlight.ai>
2024-02-17 21:54:20 -0800

    Slightly better error message.

--- src/glcontrol/cfgtools/base.py
+++ src/glcontrol/cfgtools/base.py
@@ -24,7 +24,7 @@ def pydantic_err_to_friendly(err: ValidationError) -> list[str]:
     return msgs
 
 
-def substitute_variables(raw_str: str) -> str:
+def substitute_variables(raw_str: str, filename: str) -> str:
     """Substitutes environment variables in the raw string.
     Looks for patterns like {{VARNAME}} and replaces it with the value of the environment variable VARNAME.
     """
@@ -38,7 +38,7 @@ def substitute_variables(raw_str: str) -> str:
             env_value = os.environ[var_name]
             result = result.replace("{{" + var_name + "}}", env_value)
         else:
-            raise ValueError(f"No environment variable found for: {var_name}")
+            raise ValueError(f"Missing environment variable {var_name} which is referenced in {filename}")
     return result
     
 
@@ -48,14 +48,14 @@ class Parseable:
     """A mixin for pydantic BaseModel that adds a parse method from a file."""
 
     @classmethod
-    def from_file(cls, config_fn: str) -> "Parseable":
+    def from_file(cls, config_path: str) -> "Parseable":
         """Loads the config file which is YAML and returns the parsed config."""
-        with open(config_fn, "r") as f:
+        with open(config_path, "r") as f:
             # first load it as a string
             raw_str = f.read()
 
         # Now call variable substitution
-        raw_str = substitute_variables(raw_str)
+        raw_str = substitute_variables(raw_str, config_path)
 
         # Now parse it into a dict
         raw = yaml.safe_load(raw_str)
@@ -64,6 +64,6 @@ class Parseable:
             return cls(**raw)
         except ValidationError as e:
             msg = "\n".join(pydantic_err_to_friendly(e))
-            raise ParsingError(f"Failed to parse {config_fn}: {msg}") from e
+            raise ParsingError(f"Failed to parse {config_path}: {msg}") from e
         except Exception as e:
-            raise ParsingError(f"Failed to parse {config_fn}: {e}") from e
+            raise ParsingError(f"Failed to parse {config_path}: {e}") from e
